e432a11c19bb9dea876a17bf1094ca4532900e31
rymasc <ryan.matt.scott@gmail.com>
2020-04-04 15:53:48 -0400

    Pong app skeleton

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import kivy
 kivy.require('1.11.1')
 from kivy.app import App
-from kivy.uix.gridlayout import  GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
-class LoginScreen(GridLayout):
-    
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+class PongGame(Widget):
+    pass
 
-class MyApp(App):
+class PongApp(App):
 
     def build(self):
-        return LoginScreen()
+        return PongGame()
 
 if __name__ == '__main__':
-    MyApp().run()
+    PongApp().run()
